8de6d2495ea0ccc60b6b93a696460febfacfc45d
murabye <vlada_kuz2011@mail.ru>
2017-06-04 15:07:41 +0500

    черновый вариант

--- ex7/Program.cs
+++ ex7/Program.cs
@@ -5,16 +5,18 @@ namespace ex7
 {
     internal class Program
     {
+        static int[] alphabet = new int[0];
+        static int lex = 0, length = 0;
+
         private static void Main()
         {
             while (true)
             {
-                int length = Ask.Num("Введите количество элементов (длину слова): "),
+                length = Ask.Num("Введите количество элементов (длину слова): ");
                 lex = Ask.Num("Введите емкость алфавита: ");
-
                 Console.WriteLine();
 
-                var alphabet = new int[lex];
+                alphabet = new int[lex];                    // инициализация алфавита
                 
                 for (var i = 0; i < lex; i++)
                     alphabet[i] = Ask.Num(String.Format("Введите {0} число: ", i+1));
@@ -22,15 +24,26 @@ namespace ex7
                 var num = Math.Pow(lex, length);            // количество комбинаций
 
                 for (var i = 0; i < num; i++)
-                {
-                    
-                }
-
-                // перевод в систему счисления
-                Convert.ToString(1, 3);
-
+                    CreateWord(i);
+                
                 OC.Stay();
             }
+        }
+
+       static void CreateWord(int num)
+        {
+            var inSystem = Convert.ToString(num, lex);              // перевели в систему счисления
+            const char start = '0';                                 // переменная для начала отсчета
+            
+            inSystem = inSystem.PadLeft(length).Replace(" ", "0");  // добавление незначащих нулей в начале
+
+            for (var i = 0; i < length; i++)
+            {
+                var index = (int)inSystem[i] - (int)start;
+                Console.Write(alphabet[index]);
+            }
+
+            Console.Write(", ");
         }
     }
 }

137f64fd30f1ea36045e40d5ef316aa8e24f96b1
murabye <vlada_kuz2011@mail.ru>
2017-06-04 14:35:38 +0500

    вариант через системы счисления

--- ex7/Program.cs
+++ ex7/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MyLib;
 
 namespace ex7
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            while (true)
+            {
+                int length = Ask.Num("Введите количество элементов (длину слова): "),
+                lex = Ask.Num("Введите емкость алфавита: ");
+
+                Console.WriteLine();
+
+                var alphabet = new int[lex];
+                
+                for (var i = 0; i < lex; i++)
+                    alphabet[i] = Ask.Num(String.Format("Введите {0} число: ", i+1));
+                
+                var num = Math.Pow(lex, length);            // количество комбинаций
+
+                for (var i = 0; i < num; i++)
+                {
+                    
+                }
+
+                // перевод в систему счисления
+                Convert.ToString(1, 3);
+
+                OC.Stay();
+            }
         }
     }
 }

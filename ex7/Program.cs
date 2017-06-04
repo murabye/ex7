f3376d97518ac4154640249e63b3d04f22db0eb3
murabye <vlada_kuz2011@mail.ru>
2017-06-04 15:26:38 +0500

    Доделана С учетом отсутствия встроенного перевода

--- ex7/Program.cs
+++ ex7/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using MyLib;
 
+// нет проверки на повторяемость при вводе алфавита
+// размещаем только цифры
+
 namespace ex7
 {
     internal class Program
     {
-        static int[] alphabet = new int[0];
-        static int lex = 0, length = 0;
+        static int[] alphabet = new int[0];                 // алфавит
+        static int lex = 0,                                 // основание системы счисления или емкость алф
+            length = 0;                                     // длина слова
 
         private static void Main()
         {
@@ -18,7 +22,7 @@ namespace ex7
 
                 alphabet = new int[lex];                    // инициализация алфавита
                 
-                for (var i = 0; i < lex; i++)
+                for (long i = 0; i < lex; i++)
                     alphabet[i] = Ask.Num(String.Format("Введите {0} число: ", i+1));
                 
                 var num = Math.Pow(lex, length);            // количество комбинаций
@@ -32,7 +36,7 @@ namespace ex7
 
        static void CreateWord(int num)
         {
-            var inSystem = Convert.ToString(num, lex);              // перевели в систему счисления
+            var inSystem = FromDec(num, lex);                       // перевели в систему счисления
             const char start = '0';                                 // переменная для начала отсчета
             
             inSystem = inSystem.PadLeft(length).Replace(" ", "0");  // добавление незначащих нулей в начале
@@ -43,7 +47,21 @@ namespace ex7
                 Console.Write(alphabet[index]);
             }
 
-            Console.Write(", ");
+            Console.Write(" ");
         }
+
+         static string FromDec(int num, int lex)
+        {
+            var result = "";                                        // результат
+
+            for (; num > 0; num /= lex)                             // делить на основание системы
+            {
+                var x = num % lex;                                  // остаток от деления
+                result = (char)(x < 0 || x > 9 ? x + 'A' - 10 : x + '0') + result;
+            }
+
+            return result;
+        }
+
     }
 }
